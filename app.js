9095b6100f2c5f18a1e75cd589297d02ef3279ae
hritik pandey <128885231+hri1234@users.noreply.github.com>
2024-03-27 15:24:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ let allowedOrigins = [
   "http://localhost:5000",
   "https://pososf.onrender.com",
 "https://pososf.netlify.app",
-  "http://localhost:3001"
+  "http://localhost:3001",
+  "https://inventory-backend-8r4e.onrender.com"
 
 ];
 
